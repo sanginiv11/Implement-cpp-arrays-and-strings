653e1605d9c3ea5a9b0c2fda500a03dc1c64baf6
Sangini Verma <sanginiv11@gmail.com>
2025-08-07 00:34:14 +0530

    Add files via upload

---
+++ findnuminarray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[5], num, found = 0;
+    cout << "Enter 5 integers: ";
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter number to search: ";
+    cin >> num;
+
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] == num) {
+            found = 1;
+            break;
+        }
+    }
+
+    if(found)
+        cout << num << " is present in the array." << endl;
+    else
+        cout << num << " is not present in the array." << endl;
+
+    return 0;
+}
