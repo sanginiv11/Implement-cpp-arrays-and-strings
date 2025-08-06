e8ac83c876788db6b2f362350627fd725e810ed0
Sangini Verma <sanginiv11@gmail.com>
2025-08-07 00:28:06 +0530

    Add files via upload

---
+++ printarrayelements.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[5];
+    cout << "Enter 5 integers: ";
+    
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Array elements are: ";
+    
+    for(int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    cout << endl;
+    return 0;
+}
